99698a202d58dcf5cf84b1c0dadabcfeeca57db7
innoflash <innocentmazando@gmail.com>
2019-10-25 17:29:11 +0200

    initial commit

---
+++ src/Providers/MessagesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FaithGen\Messages\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MessagesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/database/migrations/2019_09_04_144550_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->string('id', 150)->primary();
+            $table->string('ministry_id', 150)->index();
+            $table->string('title');
+            $table->text('message');
+            $table->timestamps();
+
+            $table->foreign('ministry_id')->references('id')->on('ministries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
