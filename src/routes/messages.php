59c27df268ee4dd1fde99a85a2db423bcb44d790
innoflash <innocentmazando@gmail.com>
2019-12-11 10:31:52 +0200

    refactored routes

--- src/routes/messages.php
+++ src/routes/messages.php
@@ -1,5 +1,8 @@
 <?php
 
+use FaithGen\Messages\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
+
 Route::name('messages.')->prefix('messages/')->group(function () {
-    Route::get('/', 'MessageController@index');
+    Route::get('/', [MessageController::class, 'index']);
 });
--- src/routes/source.php
+++ src/routes/source.php
@@ -1,6 +1,10 @@
 <?php
+
+use FaithGen\Messages\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
+
 Route::name('messages.')->prefix('messages/')->group(function () {
-    Route::post('/create', 'MessageController@create')->middleware('source.site');
-    Route::post('/update', 'MessageController@update')->middleware('source.site');
-    Route::delete('/delete', 'MessageController@destroy')->middleware('source.site');
+    Route::post('/create', [MessageController::class, 'create'])->middleware('source.site');
+    Route::post('/update', [MessageController::class, 'update'])->middleware('source.site');
+    Route::delete('/delete', [MessageController::class, 'destroy'])->middleware('source.site');
 });
