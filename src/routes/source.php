e70f20e5670b852108472743384b92188a066a20
innoflash <innocentmazando@gmail.com>
2020-02-10 17:50:02 +0200

    added packages

--- src/routes/source.php
+++ src/routes/source.php
@@ -3,8 +3,11 @@
 use FaithGen\Messages\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('messages.')->prefix('messages/')->group(function () {
-    Route::post('/create', [MessageController::class, 'create'])->middleware('source.site');
-    Route::post('/update', [MessageController::class, 'update'])->middleware('source.site');
-    Route::delete('/delete', [MessageController::class, 'destroy'])->middleware('source.site');
-});
+Route::name('messages.')
+    ->prefix('messages/')
+    ->middleware('source.site')
+    ->group(function () {
+        Route::post('/create', [MessageController::class, 'create']);
+        Route::post('/update', [MessageController::class, 'update']);
+        Route::delete('/delete', [MessageController::class, 'destroy']);
+    });
