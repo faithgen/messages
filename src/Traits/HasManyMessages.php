ef69a66921f0cac622f412df2ad43e9b42426f9e
innoflash <innocentmazando@gmail.com>
2020-03-03 09:13:12 +0200

    added many messages trait

---
+++ src/Traits/HasManyMessages.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace FaithGen\Messages\Traits;
+
+
+use FaithGen\Messages\Models\Message;
+
+trait HasManyMessages
+{
+    /**
+     * Relates many messages to a given model
+     *
+     * @return mixed
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+}
