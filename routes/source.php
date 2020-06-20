68c9cf0c3f17b5b66fc615699542d5a29b05c0fb
innoflash <innocentmazando@gmail.com>
2020-06-20 04:24:47 +0200

    updated message creating

--- routes/source.php
+++ routes/source.php
@@ -7,7 +7,7 @@ Route::name('messages.')
     ->prefix('messages/')
     ->middleware('source.site')
     ->group(function () {
-        Route::post('/create', [MessageController::class, 'create']);
-        Route::post('/update/{message}', [MessageController::class, 'update']);
+        Route::post('', [MessageController::class, 'create']);
+        Route::post('update/{message}', [MessageController::class, 'update']);
         Route::delete('/delete', [MessageController::class, 'destroy']);
     });
